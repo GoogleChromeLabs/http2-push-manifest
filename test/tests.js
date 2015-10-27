35dcf90f0a1380a333e8698d4a719f02f4f7caf9
Eric Bidelman <ebidel@gmail.com>
2015-10-27 13:02:09 -0700

    Add more tests and es6 usage

--- test/tests.js
+++ test/tests.js
@@ -5,6 +5,31 @@ let assert = require('assert');
 let fs = require('fs');
 
 const oldOld = console.log;
+const EXPECTED_FILE = {
+  "/css/app.css": {
+    "weight": 1,
+    "type": "style"
+  },
+  "/js/app.js": {
+    "weight": 1,
+    "type": "script"
+  },
+  "/doesntexist.json": {
+    "weight": 1,
+    "type": "script"
+  },
+  "/api/endpoint": {
+    "weight": 1
+  },
+  "/subimport.html": {
+    "weight": 1,
+    "type": "document"
+  },
+  "/import.html": {
+    "weight": 1,
+    "type": "document"
+  }
+};
 
 function muteLogger() {
   console.log = function() {}
@@ -46,38 +71,43 @@ suite('manifest.js', () => {
   });
 
   test('list resources', done => {
-    let fs = require('fs');
-
-    var expectedFileOutput = {
-      '/css/app.css': 1,
-      '/js/app.js': 1,
-      '/subimport.html': 1,
-      '/import.html': 1
-    };
 
     listresources(manifest).then(list => {
-      var name = manifest.DEFAULT_MANIFEST_NAME;
+      let name = manifest.DEFAULT_MANIFEST_NAME;
+      let urls = Object.keys(EXPECTED_FILE);
 
-      assert.equal(
-          list.length, Object.keys(expectedFileOutput).length,
-          'found all resources');
+      assert.equal(list.length, urls.length, 'found all resources');
 
       fs.readFile(name, (err, data) => {
         if (err) {
           throw err;
         }
+
+        var json = JSON.parse(data);
+
+        assert(!json['https://example.com/json'], 'External files are left out');
+        assert(EXPECTED_FILE['/doesntexist.json'], 'non-existent local resources are included');
+        assert(EXPECTED_FILE['/api/endpoint'], 'url without file extension is included');
+
         // Node 4.2.1 doesn't support ...[] yet. Built ourself.
-        var arr = Object.keys(expectedFileOutput).concat(
-            Object.keys(JSON.parse(data)));
+        let arr = urls.concat(Object.keys(json));
         let union = new Set(arr);
-        assert.equal(
-            union.size, Object.keys(expectedFileOutput).length,
-            'all resources written to file');
+        assert.equal(union.size, urls.length, 'all resources written to file');
+
+        // Node 4.2.1 doesn't support for...of
+        for (let key in json) {
+          assert('type' in json[key], '.type property exists for all urls');
+          if (key === '/api/endpoint') {
+            assert.equal(json[key].type, '', '.type is empty for urls without file extensions');
+          }
+        }
 
         fs.unlinkSync(name); // cleanup
 
         done();
       });
+    }).catch(err => {
+      console.log(err);
     });
   });
 
